b3c7bebc293dce6e3d6f40f552d51a0c67bd4307
phamtanphat <phatdroid94@gmail.com>
2022-02-15 18:31:10 +0700

    update

--- app/src/main/java/com/example/broadcastreceiver30112021/MainActivity.java
+++ app/src/main/java/com/example/broadcastreceiver30112021/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.broadcastreceiver30112021;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
     }
 }
--- app/src/main/java/com/example/broadcastreceiver30112021/MyBroadcastReceiver.java
+++ app/src/main/java/com/example/broadcastreceiver30112021/MyBroadcastReceiver.java
@@ -1,12 +1,43 @@
 package com.example.broadcastreceiver30112021;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.util.Log;
 
-public class MyBroadcastReceiver extends BroadcastReceiver {
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+
+public class MyBroadcastReceiver extends BroadcastReceiver implements LocationListener {
+    LocationManager mLocationManager;
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        mLocationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, this);
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        Log.d("BBB","Lat : " + location.getLatitude()  + " , lon " + location.getLongitude());
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
 
     }
 }
