d0131a4968f0177fba3a4894b1b3a94bc647ae28
phamtanphat <phatdroid94@gmail.com>
2022-02-15 18:43:24 +0700

    test api duoi 26

--- app/src/main/java/com/example/broadcastreceiver30112021/MainActivity.java
+++ app/src/main/java/com/example/broadcastreceiver30112021/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.broadcastreceiver30112021;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +19,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},
+                    123
+            );
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 123){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(MainActivity.this, "Đã cấp quyền", Toast.LENGTH_SHORT).show();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
--- app/src/main/java/com/example/broadcastreceiver30112021/MyBroadcastReceiver.java
+++ app/src/main/java/com/example/broadcastreceiver30112021/MyBroadcastReceiver.java
@@ -8,27 +8,31 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 
 public class MyBroadcastReceiver extends BroadcastReceiver implements LocationListener {
     LocationManager mLocationManager;
+    Context context;
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        this.context = context;
         mLocationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
 
         if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
-        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, this);
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, this);
     }
 
     @Override
     public void onLocationChanged(@NonNull Location location) {
-        Log.d("BBB","Lat : " + location.getLatitude()  + " , lon " + location.getLongitude());
+        Toast.makeText(context, "Lat : " + location.getLatitude() + " , lon " + location.getLongitude(), Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -38,6 +42,11 @@ public class MyBroadcastReceiver extends BroadcastReceiver implements LocationLi
 
     @Override
     public void onProviderDisabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
 
     }
 }
